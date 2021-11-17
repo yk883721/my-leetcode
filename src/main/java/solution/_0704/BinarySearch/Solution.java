74d77393d77691c541be1aa21507b7778c9df25e
kang <1037650891@qq.com>
2021-11-17 23:23:06 +0800

    1、add 704，二分查找添加非递归解法

--- src/main/java/solution/_0704/BinarySearch/Solution.java
+++ src/main/java/solution/_0704/BinarySearch/Solution.java
@@ -3,13 +3,26 @@ package solution._0704.BinarySearch;
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(new Solution().search(new int[]{-1,0,3,5,9,12},9));
+        System.out.println(new Solution().search(new int[]{-1,0,3,5,9,12},111));
     }
 
     public int search(int[] nums, int target) {
-        return search(nums, 0, nums.length - 1, target);
+        // 解法二，直接非递归实现二分查找
+        int l = 0, r = nums.length - 1;
+        while (l <= r){
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target){
+                return mid;
+            }else if (nums[mid] < target){
+                l = mid + 1;
+            }else {
+                r = mid -1;
+            }
+        }
+        return -1;
     }
 
+    // 解法一，递归实现二分查找
     private int search(int[] nums, int l, int r, int target){
         if (l > r){
             return -1;
