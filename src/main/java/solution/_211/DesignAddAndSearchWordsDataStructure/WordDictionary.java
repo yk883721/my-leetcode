656e6e0d5848b970c9b43bc0c30a861b2f7a3d5e
kang <1037650891@qq.com>
2021-12-02 23:31:06 +0800

    1、208,211,677 - Trie 前缀树解决问题

---
+++ src/main/java/solution/_211/DesignAddAndSearchWordsDataStructure/WordDictionary.java
@@ -0,0 +1,77 @@
+package solution._211.DesignAddAndSearchWordsDataStructure;
+
+import java.util.TreeMap;
+
+public class WordDictionary {
+
+    Node root;
+
+    public WordDictionary() {
+        root = new Node();
+    }
+
+    public void addWord(String word) {
+        Node p = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            p.next.putIfAbsent(c, new Node());
+            p = p.next.get(c);
+        }
+        p.isWord = true;
+    }
+
+    public boolean search(String word) {
+        return search(root, word, 0);
+    }
+
+    public boolean search(Node node, String word, int index) {
+        if (index == word.length()){
+            return node.isWord;
+        }
+
+        char c = word.charAt(index);
+        if ('.' == c){
+            for (Node childNode : node.next.values()) {
+                if (search(childNode, word, index + 1)){
+                    return true;
+                }
+            }
+            return false;
+        }else {
+            if (!node.next.containsKey(c)){
+                return false;
+            }
+            return search( node.next.get(c), word,index + 1);
+        }
+    }
+
+    static class Node{
+
+        boolean isWord;
+        TreeMap<Character, Node> next;
+
+        public Node() {
+            this(false);
+        }
+
+        public Node(boolean isWord) {
+            this.isWord = isWord;
+            this.next = new TreeMap<>();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        WordDictionary wordDictionary = new WordDictionary();
+        wordDictionary.addWord("bad");
+        wordDictionary.addWord("dad");
+        wordDictionary.addWord("mad");
+        System.out.println(wordDictionary.search("pad"));// return False
+        System.out.println(wordDictionary.search("bad"));// return True
+        System.out.println(wordDictionary.search(".ad")); // return True
+        System.out.println(wordDictionary.search("b..")); // return True
+
+    }
+
+}
