d1a5c0d86a253cad542b3b37d36fda984b29ae48
kang <1037650891@qq.com>
2021-12-04 00:33:10 +0800

    1、547 并查集 quickUnion 解法

--- src/main/java/solution/_0547/NumberOfProvinces/Solution.java
+++ src/main/java/solution/_0547/NumberOfProvinces/Solution.java
@@ -4,37 +4,107 @@ import java.util.TreeSet;
 
 public class Solution {
 
-    public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-        int[] id = new int[n];
-        for (int i = 0; i < id.length; i++) {
-            id[i] = i;
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (isConnected[i][j] == 1){
-                    int pId = id[i];
-                    int qId = id[j];
+    //region 解法一：quickFind
+    //    public int findCircleNum(int[][] isConnected) {
+//        int n = isConnected.length;
+//        int[] id = new int[n];
+//        for (int i = 0; i < id.length; i++) {
+//            id[i] = i;
+//        }
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < i; j++) {
+//                if (isConnected[i][j] == 1){
+//                    int pId = id[i];
+//                    int qId = id[j];
+//
+//                    if (pId != qId){
+//                        for (int k = 0; k < id.length; k++) {
+//                            if (id[k] == pId){
+//                                id[k] = qId;
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//
+//        TreeSet<Integer> set = new TreeSet<>();
+//        for(int i = 0 ; i < n ; i ++)
+//            set.add(id[i]);
+//        return set.size();
+//    }
+    //endregion
 
-                    if (pId != qId){
-                        for (int k = 0; k < id.length; k++) {
-                            if (id[k] == pId){
-                                id[k] = qId;
-                            }
-                        }
-                    }
-                }
+    //region 解法二：quickUnion
+    private interface UF {
+        int getSize();
+        boolean isConnected(int p, int q);
+        void unionElements(int p, int q);
+    }
+    private class QuickUnion implements UF{
+
+        // 我们的第二版Union-Find, 使用一个数组构建一棵指向父节点的树
+        // parent[i]表示第一个元素所指向的父节点
+        private int[] parent;
+
+        public QuickUnion(int size){
+            parent = new int[size];
+            for (int i = 0; i < parent.length; i++) {
+                parent[i] = i;
             }
         }
-        
+
+        @Override
+        public int getSize() {
+            return parent.length;
+        }
+
+        @Override
+        public boolean isConnected(int p, int q) {
+            int pId = find(p);
+            int qId = find(q);
+            return qId == pId;
+        }
+
+        @Override
+        public void unionElements(int p, int q) {
+            int pId = find(p);
+            int qId = find(q);
+
+            if (pId == qId){
+                return;
+            }
+            parent[pId] = qId;
+        }
+
+        private int find(int index){
+            if(index < 0 || index >= parent.length)
+                throw new IllegalArgumentException("p is out of bound.");
+            while (parent[index] != index){
+                index = parent[index];
+            }
+            return index;
+        }
+
+    }
+    public int findCircleNum(int[][] M) {
+
+        int n = M.length;
+        QuickUnion uf = new QuickUnion(n);
+        for(int i = 0 ; i < n ; i ++)
+            for(int j = 0 ; j < i ; j ++)
+                if(M[i][j] == 1)
+                    uf.unionElements(i, j);
+
         TreeSet<Integer> set = new TreeSet<>();
         for(int i = 0 ; i < n ; i ++)
-            set.add(id[i]);
+            set.add(uf.find(i));
         return set.size();
     }
+    //endregion
 
     public static void main(String[] args) {
-        int[][] nums = new int[][]{{1,0,0},{0,1,0},{0,0,1}};
+        int[][] nums = new int[][]{{1,1,0},{1,1,0},{0,0,1}};
         System.out.println(new Solution().findCircleNum(nums));
     }
 
